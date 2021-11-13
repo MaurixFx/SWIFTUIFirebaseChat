582b418657366df91158224b783ac3455e6e8aa9
Mauricio Figueroa <mauriciofigueroa@Mauricios-MacBook-Pro.local>
2021-11-13 15:48:08 -0300

    First commit

---
+++ Shared/SwiftUIFirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseChatApp.swift
+//  Shared
+//
+//  Created by Mauricio Figueroa on 13-11-21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
