6aa3c611d6ad356d73e2c90c0aa61d6c591f8154
Mauricio Figueroa <m.figueroa.olivares@live.cl>
2021-11-20 09:25:28 -0300

    Refactor in chat user model. move logic of assignation of data

--- Shared/Modules/MainMessages/Model/ChatUser.swift
+++ Shared/Modules/MainMessages/Model/ChatUser.swift
@@ -12,4 +12,11 @@ struct ChatUser {
     let email: String
     let profileImageUrl: String
     let username: String
+    
+    init(data: [String: Any]) {
+        self.uid = data["uid"] as? String ?? ""
+        self.email = data["email"] as? String ?? ""
+        self.profileImageUrl = data["profileImageUrl"] as? String ?? ""
+        self.username = data["username"] as? String ?? ""
+    }
 }
